1d021e37ee818585978e4a4930b4b1beb673f9ca
qudcks <akftod4007@naver.com>
2022-11-29 17:08:14 +0900

    feat goods

---
+++ main/routes/goods.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bcrypt = require('bcrypt')
+const User = require('../models/goods');
+
+const router = express.Router();
+
+router.route('/')
+    .get(async (req, res, next) => {
+        try {
+            const users = await User.findAll({
+                attributes: ['id']
+            });
+            res.locals.title = require('../package.json').name;
+            res.locals.port = process.env.PORT;
+            res.locals.users = users.map(v => v.id);
+            res.render('user');
+        } catch (err) {
+            console.error(err);
+            next(err);
+        }
+    })
+
+
+module.exports = router;
