b997972fa7e238e2a7695c913fdda73d03b80456
qudcks <akftod4007@bible.ac.kr>
2022-11-29 20:13:42 +0900

    add: cart/search router

---
+++ main/routes/cart.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+const express = require('express');
+const Cart = require('../models/cart');
+
+const { isLoggedIn } = require('./helpers');
+
+
+const router = express.Router();
+
+router.route('/')
+    .get(isLoggedIn, (req, res) => {
+        res.locals.title = require('../package.json').name;
+        res.locals.userId = req.user.id;
+        res.render('cart');
+    })
+    .post(async (req, res, next) => {
+        const { cart } = req.body;
+        const userId = req.user.id;
+
+        try {
+            await Cart.create({ userId, cart });
+            res.redirect('/');
+        } catch (err) {
+            console.error(err);
+            next(err);
+        }
+    });
+
+//장바구니 전체 조회
+router.get('/carts', async (req, res, next) => {});
+
+//삭제
+router.get('/carts/:id', async (req, res, next) => {});
+
+//물건 개수 변경 update
+router.post('/carts/:id', async (req, res, next) => {});
+
+module.exports = router;
