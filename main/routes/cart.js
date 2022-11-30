c2ff8b46a58afc256631c54b26739768e6a5153f
max990624 <max990624@naver.com>
2022-12-01 00:01:17 +0900

    fix: cart router to realese

--- main/routes/cart.js
+++ main/routes/cart.js
@@ -8,17 +8,6 @@ const { isLoggedIn } = require('./helpers');
 
 const router = express.Router();
 
-<<<<<<< HEAD
-// 장바구니 전체조회
-router.get('/carts', async (req, res, next) => {});
-
-// 장바구니 물품 삭제
-// get 대신에 delete 쓰면 안되나?
-router.get('/carts/:id', async (req, res, next) => {});
-
-// 장바구니 물품 개수 변경
-router.post('/carts/:id', async (req, res, next) => {});
-=======
 router.route('/')
     .get(isLoggedIn, (req, res) => {
         res.locals.title = require('../package.json').name;
@@ -46,6 +35,5 @@ router.get('/delete/:id', async (req, res, next) => {});
 
 //물건 개수 변경 update
 router.post('/update/:id', async (req, res, next) => {});
->>>>>>> bc
 
 module.exports = router;
