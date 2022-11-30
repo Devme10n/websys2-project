f83f4b3274ca13ec5c3190da1a383be76df933df
qudcks <akftod4007@bible.ac.kr>
2022-11-30 17:21:47 +0900

    fix: order.router

--- main/routes/order.js
+++ main/routes/order.js
@@ -14,7 +14,7 @@ router.get('/parchase/point/form', async (req, res, next) => {});
 //포인트 결제 방법 post
 router.post('/parchase/point', async (req, res, next) => {});
 //포인트 선물 결제 방법 post
-router.post('/present/parchase/point', async (req, res, next) => {});
+router.post('/parchase/present/point', async (req, res, next) => {});
 
 //카드 결제 방법 폼
 //질문: 카드사 별로 전략을 사용하고 싶음
@@ -23,4 +23,4 @@ router.get('/parchase/card/form', async (req, res, next) => {});
 //카드 결제 방법
 router.post('/parchase/card', async (req, res, next) => {});
 //카드 선물 결제 방법
-router.post('/present/parchase/card', async (req, res, next) => {});
+router.post('/parchase/present/card', async (req, res, next) => {});
