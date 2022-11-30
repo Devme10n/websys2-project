112574e5b75c581ce50060ef92ef0d2aacf9c9b4
max990624 <max990624@naver.com>
2022-12-01 00:25:05 +0900

    fix: carts router

--- main/routes/carts.js
+++ main/routes/carts.js
@@ -13,21 +13,49 @@ const router = express.Router();
 req: req.user.id
 res: 장바구니 목록, res.locals.carts = {} // 리스트? 객체?
  */
-router.get('/', async (req, res, next) => {});
+router.get('/', async (req, res, next) => {
+
+});
 
 //삭제
 /** 
-req:
-res:
+req: req.params.id
+res: delete 여부
  */
-router.get('/delete/:id', async (req, res, next) => {});
+router.get('/delete/:id', async (req, res, next) => {
+    try {
+        const result = await Carts.destroy({
+            where: { id: req.params.id }
+        });
+
+        if (result) res.redirect('/'); // ?
+        else next('Not deleted!')
+    } catch (err) {
+        console.error(err);
+        next(err);
+    }
+});
 
 //물건 개수 변경 update
 /** 
-req: 
-res: 
+req: req.body.id
+res: update 여부
 body: 
  */
-router.post('/update/:id', async (req, res, next) => {});
+router.post('/update/:id', async (req, res, next) => {
+    try {
+        const result = await Carts.update({
+            description: req.body.description // description -> 개수(count?)
+        }, {
+            where: { id: req.body.id }
+        });
+
+        if (result) res.redirect('/');
+        else next('Not updated!')
+    } catch (err) {
+        console.error(err);
+        next(err);
+    }
+});
 
 module.exports = router;
