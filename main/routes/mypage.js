83648c8f08a82947a86969ef9ab4ea84452cbd0c
qudcks <akftod4007@bible.ac.kr>
2022-12-03 00:06:25 +0900

    fix: mypage.router

--- main/routes/mypage.js
+++ main/routes/mypage.js
@@ -21,16 +21,40 @@ req: req.params.id 로그인된 아이디
 res: 로그인된 유저의 pick 객체
  */
 router.get('/pick', async (req, res, next) => {
+    try {
+        //product에서 product id로 가져오기
+        const pick = await Pick.findAll({        
+            where: { userId: req.user.id }
+        });
+        res.json(pick);
+    } catch (err) {
+        console.error(err);
+        next(err);
+    }
 });
 //찜 목록 삭제
 /** 
 req: req.params.id
 res: 로그인된 유저의 pick의 goods=:id 인 물품 삭제
  */
-router.get('/pick/delete/:id', async (req, res, next) => {
+router.get('/pick/delete/product/:id', async (req, res, next) => {
     try {
         const result = await Pick.destroy({
-            where: { id: req.params.id }
+            where: { productId: req.params.id }
+        });
+
+        if (result) res.redirect('/');
+        else next('Not deleted!')
+    } catch (err) {
+        console.error(err);
+        next(err);
+    }
+});
+//세트 찜 목록 삭제
+router.get('/pick/delete/package/:id', async (req, res, next) => {
+    try {
+        const result = await Pick.destroy({
+            where: { packageId: req.params.id }
         });
 
         if (result) res.redirect('/');
@@ -45,15 +69,46 @@ router.get('/pick/delete/:id', async (req, res, next) => {
 //구매 기록 조회
 /**
 req:: req.params.id
-res:: 로그인된 유저의 구매기록 조회
+res:: 로그인된 유저의 주문기록 전체 조회
  */
-router.get('/orders', async (req, res, next) => {});
+router.get('/orders', async (req, res, next) => {
+    try {
+        //Order에서 userid로 가져오기
+        const orders = await Order.findAll({
+            where: { userId: req.user.id }
+        });
+        res.json(orders);
+    } catch (err) {
+        console.error(err);
+        next(err);
+    }
+});
+
+///get 주문 조회-/mypage/order
+/**
+req: req.goods.id (로그인된 유저가 주문할 상품 id)
+res: 로그인 된 유저가 주문한 물품의 배송 상태
+ */
+router.get('/order/:id', async (req, res, next) => {
+    try {
+        //Order에서 orderid로 가져오기
+        const order = await Order.findAll({
+            where: { id: req.params.id }
+        });
+        res.json(order);
+    } catch (err) {
+        console.error(err);
+        next(err);
+    }
+});
+
+
 //구매 기록 삭제
 router.get('/order/delete/:id', async (req, res, next) => {
     try {
-        //orders 아이디로 검색해서 Orders에서 삭제
+        //order id로 검색해서 Order에서 삭제
         const result = await Orders.destroy({
-            where: { id: req.params.id }
+            where: { orderId: req.params.id }
         });
 
         if (result) res.redirect('/');
@@ -63,22 +118,32 @@ router.get('/order/delete/:id', async (req, res, next) => {
         next(err);
     }
 });
-///get 배송 조회-/mypage/order
-/**
-req: req.goods.id (로그인된 유저가 주문할 상품 id)
-res: 로그인 된 유저가 주문한 물품의 배송 상태
- */
-router.get('/order/:id', async (req, res, next) => {});
 
 //물품 교환 페이지 폼
-router.get('/exchange/:id/form', async (req, res, next) => {});
+router.get('/exchange/:id/form', async (req, res, next) => {
+
+});
 //물품 교환
 /**
 req: 배송 전인 물품의 id, 변경할 물품의 id
 res: 물품이 배송 전이라면  교환 (200)/ 물품이 배송 중이라면 실패()
 body: 배송상태의 변경
  */
-router.post('/exchange/:id', async (req, res, next) => {});
+router.post('/exchange', async (req, res, next) => {
+    try {
+        const orderId = req.user.id;
+        const result = await Order.update({ state: req.body.state}, 
+        {
+            where: { id: req.body.id }
+        });
+
+        if (result) res.redirect('/');
+        else next('Not updated!')
+    } catch (err) {
+        console.error(err);
+        next(err);
+    }
+});
 
 //환불 페이지 폼
 // RESTful한가?
@@ -87,19 +152,45 @@ router.get('/refund/:id/form', async (req, res, next) => {});
 /** :id 는 goods 상품 번호
 body: 변경된 상품 번호
 */
-router.post('/refund/:id', async (req, res, next) => {});
+router.post('/refund', async (req, res, next) => {
+    try {
+        const result = await Order.update({
+            state: req.body.state
+        }, {
+            where: { id: req.body.id }
+        });
 
-//1대1 문의 리스트
+        if (result) res.redirect('/');
+        else next('Not updated!')
+    } catch (err) {
+        console.error(err);
+        next(err);
+    }
+});
 
-router.get('/inquiry/list', async (req, res, next) => {});
+//1대1 문의 전체 조회
+router.get('/inquiry', async (req, res, next) => {
+    try {
+        //product에서 product id로 가져오기
+        const inquiry = await Inquiry.findAll({
+            attributes: ['id','userId','title','description']
+        });
+        res.json(inquiry);
+    } catch (err) {
+        console.error(err);
+        next(err);
+    }
+});
 //1대1 문의 폼
 router.get('/inquiry/form', async (req, res, next) => {});
-//1대1 문의 조회
+//1대1 문의 작성
 /**
 req: req.goods.id
 res: 로그인된 아이디가 문의한 goods.id의 1대1 문의 기록
 */
-router.post('/inquiry/:id', async (req, res, next) => {});
+router.post('/inquiry', async (req, res, next) => {
+    const { id, password, name, description } = req.body;
+});
 
 //리뷰 기록
 
@@ -112,13 +203,15 @@ response:
 body: 
 */
 router.get('/review', async (req, res, next) => {});
-//리뷰 조회
+//내가 작성한 리뷰 조회
 router.get('/review/:id', async (req, res, next) => {});
 //리뷰 작성 폼
 router.get('/review/form', async (req, res, next) => {});
-//리뷰 작성/업데이트 post
-router.post('/review/:id', async (req, res, next) => {});
-//리뷰 삭제
+//내가 장성한 리뷰 작성 post
+router.post('/review', async (req, res, next) => {});
+//내가 작성한 리뷰 update
+router.post('/review/update', async (req, res, next) => {});
+//내가 작성한 리뷰 리뷰 삭제
 router.get('/review/delete/:id', async (req, res, next) => {});
 
 //쿠폰
